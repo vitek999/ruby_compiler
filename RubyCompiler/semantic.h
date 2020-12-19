95cfebbc7fbc249e7c2ad82cbfea59f33c318563
vitek999 <vitek2012rus@gmail.com>
2020-12-19 13:55:36 +0400

    Добавил струтуры для полей, классов и т.д.

--- RubyCompiler/semantic.h
+++ RubyCompiler/semantic.h
@@ -1,6 +1,36 @@
 #pragma once
 
 #include "tree_nodes.h"
+#include "Constant.h"
+#include <string>
+#include <vector>
+#include <map>
+
+struct Field {
+	bool isStatic;
+	std::string name;
+	int number;
+};
+
+struct Method {
+	bool isStatic;
+	std::string name;
+	std::map<std::string, Field> local_variables;
+	stmt_struct* body;
+	int number;
+};
+
+class Clazz {
+public:
+	int number;
+	int parent;
+
+	std::map<int, Constant> constants;
+	std::map<std::string, Method> methods;
+	std::map<std::string, Field> fields;
+};
+
+void fillTable(program_struct* program);
 
 void transformTree(program_struct* program);
 void transform(expr_struct* expr);
